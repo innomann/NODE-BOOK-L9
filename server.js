bc0e5fc9dcce2459e25e29b6ec6e4a54a81d9ee0
Innoman <innoman.noma@gmail.com>
2020-06-21 14:17:50 +0200

    make it betterxii

--- server.js
+++ server.js
@@ -3,27 +3,20 @@ const path = require('path');
 const ejs = require('ejs');
 const app = express();
 
+const homeController = require('./controllers/home')
+const aboutController = require('./controllers/about')
+const contactController = require('./controllers/contact')
+const postController = require('./controllers/post')
+
 //EJS
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
 
 
-app.get('/', (req,res) => {
-    res.render('index')
-})
-
-app.get('/about', (req,res) => {
-    res.render('about')
-})
-
-app.get('/contact', (req,res) => {
-    res.render('contact')
-})
-
-app.get('/post', (req,res) => {
-    res.render('post')
-})
-
+app.get('/',homeController)
+app.get('/about',aboutController)
+app.get('/contact',contactController)
+app.get('/post',postController)
 
 const PORT = process.env.PORT || 7000
 app.listen(PORT,console.log(`Server running on port ${PORT}`))
